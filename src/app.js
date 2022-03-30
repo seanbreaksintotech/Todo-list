9734596401f5f7a9dfa5123864bcfc6fccbd8a8d
seanbreaksintotech <sbyrdie123@gmail.com>
2022-03-30 01:05:10 -0400

    index.html and src/app.js and style.css

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const contents = document.querySelectorAll(".content");
 const overlay = document.getElementById('overlay');
 const popup = document.getElementById('form-popup');
 
+
 //function to switch between active tabs
 tabs.forEach((tab, index) =>{
     tab.addEventListener("click", () =>{
@@ -20,6 +21,63 @@ tabs.forEach((tab, index) =>{
     });
     
 })
+
+
+//todo class
+
+class todoList{
+    constructor(title,description,dates,priority){
+        this.title = title,
+        this.description = description,
+        this.dates = dates,
+        this.priority = priority
+    }
+    
+
+}
+
+
+//function to add todo to list
+
+function addTodoToList(todo){
+    const tasklist = document.querySelector("#generalbox")
+
+    const task = document.createElement('div')
+
+    task.innerHTML = `
+    
+    <div id = "todolist">
+    <p class = "todotitle">${todo.title}</p>
+
+
+  <div id= "todos">
+ 
+        <button class="todobtn"><ion-icon name="ellipsis-horizontal"></ion-icon></button>
+   <button class="todobtn"><ion-icon name="trash-outline"></ion-icon></button>
+    
+  </div>
+
+</div>
+ `
+tasklist.appendChild(task)
+ 
+}
+
+popup.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const title = document.querySelector("#title").value
+    const description = document.querySelector('#textarea').value
+    const dates = document.querySelector('#day').value
+   
+
+
+    const todo = new todoList(title,description,dates)
+    addTodoToList(todo);
+closeForm();
+
+})
+
+
 
 //tab display on refresh
 tabs[0].click();
